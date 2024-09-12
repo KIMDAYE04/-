e737793474c1dfebebfc62eeb23de5d31f487601
Daye <kimdayea@gmail.com>
2024-09-12 17:43:54 +0900

    lst

---
+++ main.js
@@ -0,0 +1,25 @@
+document.getElementById('time').innerHTML = new Date().toLocaleString()
+
+function pink() {
+  document.body.style.backgroundColor = 'pink'
+}
+function yellow() {
+  document.body.style.backgroundColor = 'Yellow'
+}
+function lightblue() {
+  document.body.style.backgroundColor = 'Lightblue'
+}
+function reset() {
+  document.body.style.backgroundColor = 'White'
+}
+
+function showhtml() {
+  document.getElementById('fig').src = 'img/html5'
+}
+function hides() {}
+function showcss3() {
+  document.getElementById('desc').src = 'img/css3'
+}
+function showjavascript() {
+  document.getElementById('desc').src = 'img/javascipt'
+}
